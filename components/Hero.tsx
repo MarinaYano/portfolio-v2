6f94ecea22bce64b23abbc12bd7bc71a069d64ef
MarinaYano <myano2621@gmail.com>
2024-06-05 23:47:26 -0700

    fix: change padding values

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect"
 
 const Hero = () => {
   return (
-    <div className="relative pb-20 pt-36">
+    <div className="relative py-40">
 
       <div>
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
@@ -13,7 +13,7 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
