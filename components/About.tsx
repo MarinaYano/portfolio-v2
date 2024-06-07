e424c89762dec7d2cf764bbab80fdfb1d47b8d92
MarinaYano <myano2621@gmail.com>
2024-06-06 19:02:50 -0700

    add: create about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,31 @@
+import { about } from "@/data"
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid"
+import Image from "next/image"
+
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <BentoGrid className="">
+        {about.map((item, index) => (
+          <BentoGridItem
+            key={index}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            icon={item.icon && item.icon.map((icon, index) => (
+              <Image
+                key={index}
+                src={icon}
+                alt={item.title}
+                width={36}
+                height={36}
+              />
+            ))}
+            className={item.className}
+          />
+        ))}
+      </BentoGrid>
+    </div>
   )
 }
 
