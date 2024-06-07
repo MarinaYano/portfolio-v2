936db1d0de21fec17df762f271faae6a1349de68
MarinaYano <myano2621@gmail.com>
2024-06-06 19:06:32 -0700

    fix: change '' to &apos;

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,10 +6,10 @@ const Contact = () => {
     <div id="contact" className="w-full pt-20 pb-10">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[75vw]">
-          Share your goals with me, and let's make them a <span className="text-purple">reality</span>.
+          Share your goals with me, and let&apos;s make them a <span className="text-purple">reality</span>.
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Interested in working with me? Let's talk!
+          Interested in working with me? Let&apos;s talk!
         </p>
         <a href="mailto:myano2621@gmail.com">
           <MagicButton
